d47e90861f20052cd787ed42c1b45e0c301651bd
Shyam <shyam@sumedhasoftech.com>
2016-10-04 18:25:16 +0530

    04 Oct

---
+++ MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStore.Models;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            var genres = new List<Genre>
+            {
+                new Genre{Name="Disco"},
+                new Genre{Name="Jazz"},
+                new Genre{Name="Rock"}
+            };
+            return View(genres);
+        }
+        //
+        // GET: /Store/Browse?genre=Disco
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = "Genre " + genre };
+            return View(genreModel);
+        }
+        //
+        // GET: /Store/Details
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album " + id };
+            return View(album);
+        }
+
+    }
+}
